6b3f4746db43432d570d70cbb140ce7b3b856a2a
dineshkumarap <82171340+dineshkumarap@users.noreply.github.com>
2021-08-18 12:40:19 +0530

    Update SearchAlgo.c

--- SearchAlgo.c
+++ SearchAlgo.c
@@ -83,6 +83,7 @@ int main()
     printf("Enter the size of the array : ");
     scanf("%d",&n);
     int a[n];
+    printf("Enter the elements of the array : \n");
     for(int i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
